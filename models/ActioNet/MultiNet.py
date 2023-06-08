3c5b33e935171d22b7dbe2df2e48d1e3860eb73a
Teresa <teresa.argnani@gmail.com>
2023-06-08 18:59:01 +0200

    final multimodal classifier v2

--- models/ActioNet/MultiNet.py
+++ models/ActioNet/MultiNet.py
@@ -55,12 +55,10 @@ class MultiNet(nn.Module):
         # self.lstm_rgb = nn.LSTM(input_size=1024, hidden_size=1024, batch_first=True)
         
         # Fully connected layers
+        self.fc1 = nn.Linear(704 + 5 * 1024, 512)
         self.drop = nn.Dropout(p=0.15)
         self.hswish = nn.Hardswish()
-
-        self.fc1 = nn.Linear(704 + 5 * 1024, 1024)
-        self.fc2 = nn.Linear(1024, 512)
-        self.fc3 = nn.Linear(512, 21)
+        self.fc2 = nn.Linear(512, 21)
 
     def forward(self, x):
         x_rgb = x["RGB"]
@@ -82,12 +80,7 @@ class MultiNet(nn.Module):
         x = self.fc1(x)
         x = self.drop(x)
         x = self.hswish(x)
-
         x = self.fc2(x)
-        x = self.drop(x)
-        x = self.hswish(x)
-
-        x = self.fc3(x)
         x = nn.functional.softmax(x, dim=0)
         
         return x
